8845cc9785e0e588117806f70e7d1a359873353b
asbis <rorvik.asbjorn@gmail.com>
2025-08-07 08:51:50 +0200

    Initial Commit

---
+++ KanbanBar/KanbanBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  KanbanBarApp.swift
+//  KanbanBar
+//
+//  Created by Asbjørn Rørvik on 07/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct KanbanBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KanbanBarTests/KanbanBarTests.swift
@@ -0,0 +1,17 @@
+//
+//  KanbanBarTests.swift
+//  KanbanBarTests
+//
+//  Created by Asbjørn Rørvik on 07/08/2025.
+//
+
+import Testing
+@testable import KanbanBar
+
+struct KanbanBarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
